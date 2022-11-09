4e8b200d094c55ad33112f24733f90ce9f22db38
Woobzor <vladychin.v.i.2.21@gmail.com>
2022-11-09 19:25:45 +0300

    4.2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Фредди */
+router.get('/Freddy', function(req, res, next) {
+  res.send("<h1>Страница Фредди</h1>")
+});
+
+/* Страница Бонни */
+router.get('/Bonnie', function(req, res, next) {
+  res.send("<h1>Страница Бонни</h1>")
+});
+
+/* Страница Чики */
+router.get('/Chica', function(req, res, next) {
+  res.send("<h1>Страница Чики</h1>")
+});
+
+/* Страница Фокси */
+router.get('/Foxy', function(req, res, next) {
+  res.send("<h1>Страница Фокси</h1>")
+});
+
 module.exports = router;
