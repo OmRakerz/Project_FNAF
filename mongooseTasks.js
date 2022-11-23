36f48a58e8df549b06caaebd54b42b516a2f9613
Woobzor <vladychin.v.i.2.21@gmail.com>
2022-11-23 13:13:59 +0000

    ORM

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-var Continent = mongoose.model('fnaf', { name: String })
+var fnaf = mongoose.model('fnaf', { name: String })
 
-var conti = new Continent({ name: 'Fazbear' })
-conti.save(function (err) {
+var conti = new animatronic({ name: 'Withered Foxy' })
+fnaf.save(function (err) {
     if (err) {
         console.log(err)
     } else {
