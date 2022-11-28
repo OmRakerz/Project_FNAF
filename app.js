53629c165fe7469dbc0acb0d566f3d8ba6c66530
Woobzor <vladychin.v.i.2.21@gmail.com>
2022-11-28 18:48:53 +0000

    Обработал параметр в адресе

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/fnaf')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var animatronics = require('./routes/animatronics')
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/animatronics', animatronics)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
