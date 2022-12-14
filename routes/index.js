9ca1960ec5ca5db8880071794a30e8f62abd230c
Woobzor <vladychin.v.i.2.21@gmail.com>
2022-12-14 21:44:11 +0000

    Глобальная переменная User

--- routes/index.js
+++ routes/index.js
@@ -28,24 +28,29 @@ router.post('/logreg', function(req, res, next){
   var password = req.body.password;
 })
 
+router.post('/logreg', function(req, res, next){
+  var username = req.body.username;
+  var password = req.body.password;
+
   User.findOne({username:username},function(err,user){
-  if(err) return next(err)
-  if(user){
-    if(user.checkPassword(password)){
-      req.session.user = user._id
-      res.redirect('/')
-    } 
-    else { res.render('logreg', {title: 'Вход'})
-    }
-  } else {
-    var user = new User({username:username,password:password})
-    user.save(function(err,user){
-      if(err) return next(err)
+    if(err) return next(err)
+    if(user){
+	    if(user.checkPassword(password)){
         req.session.user = user._id
         res.redirect('/')
-    })
-  }
-})
+      } 
+      else { res.render('logreg', {title: 'Вход'})
+      }
+    } else {
+	    var user = new User({username:username,password:password})
+      user.save(function(err,user){
+        if(err) return next(err)
+          req.session.user = user._id
+          res.redirect('/')
+      })
+    }
+  })
+});
 
 
 /* Страница Фредди 
