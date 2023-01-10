5864bedb2fbc686b24529de0ff89af634713bb4d
Woobzor <vladychin.v.i.2.21@gmail.com>
2023-01-10 18:55:24 +0000

    MySQL Заполнение меню из базы

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,8 +1,11 @@
-var Animatronic = require("./../models/animatronic").Animatronic
+// var Animatronic = require("./../models/animatronic").Animatronic
+var db = require('../mySQLConnect')
 
-module.exports = function(req,res,next){
+module.exports = function(req, res, next){
     res.locals.nav = []
-    Animatronic.find(null,{_id:0,title:1,nick:1},function(err,result){
+
+    // Animatronic.find(null,{_id:0,title:1,nick:1},function(err,result)
+    db.query(`SELECT title, nick FROM animatronics`, (err, result) =>{
         if(err) throw err
         res.locals.nav = result
         next()
