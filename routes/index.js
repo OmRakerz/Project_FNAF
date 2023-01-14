7792133e13bb46b9af65e9b5564550cc433e0c0d
Woobzor <vladychin.v.i.2.21@gmail.com>
2023-01-14 17:45:08 +0000

    Логика пользователя

--- routes/index.js
+++ routes/index.js
@@ -21,12 +21,32 @@ router.get('/logreg', function(req, res, next){
   res.render('logreg',{
     title: 'Вход'
   });
-})
+});
+
 
 router.post('/logreg', function(req, res, next){
   var username = req.body.username;
   var password = req.body.password;
-})
+
+  User.findOne({username:username},function(err,user){
+    if(err) return next(err)
+    if(user){
+      if(user.checkPassword(password)){
+        req.session.user = user._id
+        res.redirect('/')
+      } 
+      else { res.render('logreg', {title: 'Вход'})
+      }
+    } else {
+      var user = new User({username:username,password:password})
+      user.save(function(err,user){
+        if(err) return next(err)
+          req.session.user = user._id
+          res.redirect('/')
+      })
+    }
+  })
+});
 
 
 /* Страница Фредди 
