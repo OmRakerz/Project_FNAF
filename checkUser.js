718403f6ad517af061efaa6aff1d017e5a5fe8c7
Woobzor <vladychin.v.i.2.21@gmail.com>
2023-01-14 21:43:53 +0000

    Создание модели User

--- checkUser.js
+++ checkUser.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/all")
+mongoose.connect("mongodb://127.0.0.1:27017/fnaf")
 
 var User = require("./models/user.js").User
 
